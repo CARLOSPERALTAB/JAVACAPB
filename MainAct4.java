9a6ab0a91a4d5d2d6843b3a90d7f55bf993259e3
CARLOSPERALTAB <bohorquezpcarlosalberto@outlook.com>
2024-09-27 16:59:42 -0600

    Add files via upload

---
+++ MainAct4.java
@@ -0,0 +1,7 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class MainAct4 {
+    public static void main(String[] args) {
+       Practica_04_AddressBook.MuestraMenuAgenda();
+    }
+}
