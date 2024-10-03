d733807b2de4a75cb7787030be987a2f7e36ccdd
CARLOSPERALTAB <bohorquezpcarlosalberto@outlook.com>
2024-10-03 16:00:50 -0600

    Update Login.java

--- Login.java
+++ Login.java
@@ -7,18 +7,46 @@ public class Login {
     public static void authenticate() throws Exception {
         Scanner inputReader = new Scanner(System.in);
         String username, password;
-        System.out.print("Usuario: ");
+        System.out.print("Usuario (Nombre y apellido): ");
         username = inputReader.nextLine();
-        System.out.print("Contraseña: ");
+        System.out.print("Contraseña (8 dígitos): ");
         password = inputReader.nextLine();
-        User user = User.findByUsername(username);
-        if (user.persisted() && user.authenticate(password)) {
+
+        if(username.equals("Daniel Carmona") && password.equals("1234567890")) {
+
+                System.out.println("\n------------------");
+                System.out.println("Bienvenido Daniel Carmona");
+                System.out.println("------------------\n");
+                Menu.main();
+            }
+
+
+        if(username.equals("Luis Felipe") && password.equals("3456789012")) {
+
             System.out.println("\n------------------");
-            System.out.println("Bienvenido " + user.username);
+            System.out.println("Bienvenido Luis Felipe");
             System.out.println("------------------\n");
-        } else {
-            System.out.println("\nUsuario y/o contraseña inválida, intente nuevamente.\n");
-            authenticate();
+            Menu.main();
         }
+
+
+
+        if(username.equals("Jesus Martinez") && password.equals("5678901234")) {
+
+            System.out.println("\n------------------");
+            System.out.println("Bienvenido Jesus Martinez");
+            System.out.println("------------------\n");
+            Menu.main();
+        }
+
+
+
+
+
+            else  {
+                System.out.println("\nUsuario y/o contraseña inválida, intente nuevamente.\n");
+                authenticate();
+            }
+        }
+
     }
-}
